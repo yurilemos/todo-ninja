3525fddc665a6e42ac6da760bbdbd312af0ddc30
yurilemos <56370419+yurilemos@users.noreply.github.com>
2021-02-18 13:29:56 -0300

    commit on 'master'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores minima expedita doloribus ea, quo consequatur quod modi! Suscipit at tenetur excepturi id, provident praesentium commodi minima eos autem reprehenderit sapiente!</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, rerum blanditiis autem aliquid cumque a dolorem iste, exercitationem minus molestias tempore similique obcaecati asperiores dicta facere, dolore eaque voluptate? Vitae.</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller display</h4>
+    <p>This is a headline</p>
   </div>  
 </template>
 
